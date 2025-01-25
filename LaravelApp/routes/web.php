96e5ef80be9feb98e4b64a3691b1ae2958bdd3c2
pShooketh <157780588+pShooketh@users.noreply.github.com>
2025-01-25 13:26:28 +0000

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ LaravelApp/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GreetController;
+
+Route::get('/', function () {
+    return 'Hello, Laravel!';
+});
+
+Route::get('/greet', [GreetController::class, 'show']);
+
